94e8759c7f6024341ad149ae50cd893433760e0a
biggio34 <gabriel.fransen@gmail.com>
2025-05-15 16:08:46 -0500

    Create App.js

---
+++ src/App.js
@@ -0,0 +1,18 @@
+import React from 'react';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <h1>Welcome to Gabe's Coaches Corner</h1>
+      <nav>
+        <ul>
+          <li><a href="/drills">Drills</a></li>
+          <li><a href="/practice-plan">Create Practice Plan</a></li>
+        </ul>
+      </nav>
+    </div>
+  );
+}
+
+export default App;
